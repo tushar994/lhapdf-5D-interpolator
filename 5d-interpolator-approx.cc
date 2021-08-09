64dff1e79a7b29acf4828ebf6ae5d18be9801f2a
tushar994 <tushar.j@students.iiit.ac.in>
2021-08-09 14:41:28 +0530

    bugs

--- 5d-interpolator-approx.cc
+++ 5d-interpolator-approx.cc
@@ -4,7 +4,6 @@
 #include <vector>
 #include <string>
 #include <sstream>
-#include "Eigen/Eigen"
 
 // type = 0 we use the slope avg with an approximation of second derivative using only four points
 // type = 1, we use 6 points to find the second derivative
@@ -13,6 +12,7 @@ int x_size = 91;
 int q_size = 20;
 double q_arr[20], x_arr[91]; 
 double xf[20*91][3];
+// all index, get_val have been tested and are definitely correct    
 
 // This helps find the greatest x-value in the dataset below a given x value
 int x_binary(int min, int max, double val){
@@ -48,6 +48,7 @@ int q_binary(int min, int max, double val){
 int q_index(double x){
     return q_binary(0,q_size-1,x);
 }
+
 // This returns index+add, unless index+add is an invalid value,
 //  in which case it gives max or min depending on how indes+add overflowed
 int return_index(int index, int max, int min, int add){
@@ -68,11 +69,23 @@ double get_val(int x_index, int q_index, int flavour){
     int index = x_index*q_size + q_index;
     return xf[index][flavour];
 }
+
+// this is used toe subtract rows of the matrix, b asically used to solve the system of equations.
+int subtract_matrix(double equations[][7], int i, int j){
+
+    double e_i = equations[i][i];
+    double e_j = equations[j][i];
+    for(int k=0;k<7;k++){
+        equations[j][k] = equations[j][k]*e_i - equations[i][k]*e_j;
+        equations[j][k] = equations[j][k]/e_i;
+    }
+    return 1;
+}
+
 // given a system of equations, this function solves it and gives the 
 // outupt of the polynomial obtained
 double solve_system(double x,double x1, double x2, double y1, double y2, double dy1, double dy2, double ddy1, double ddy2){
-    Eigen::Matrix <double, 1,7> equations[6];
-    
+    double equations2[6][7];
     double pow_x[6];
     pow_x[0] = 1;
     for(int i=1;i<6;i++){
@@ -92,341 +105,172 @@ double solve_system(double x,double x1, double x2, double y1, double y2, double 
 
     // normal function equations
     for(int i=0;i<6;i++){
-        equations[0](0,i) = pow_x1[5-i];
+        equations2[0][i] = pow_x1[5-i];
     }
-    equations[0](0,6) = y1;
+
+    equations2[0][6] = y1;
     for(int i=0;i<6;i++){
-        equations[1](0,i) = pow_x2[5-i];
+
+        equations2[1][i] = pow_x2[5-i];
     }
-    equations[1](0,6) = y2;
+
+    equations2[1][6] = y2;
 
 
     // first derivative equations
     for(int i=0;i<6;i++){
-        if(5-i-1 > 0){
-            equations[2](0,i) = (5-i)*pow_x1[5-i-1];
+        if(5-i-1 >=  0){
+
+            equations2[2][i] = (5-i)*pow_x1[5-i-1];
         }
         else{
-            equations[2](0,i) = 0;
+
+            equations2[2][i] = 0;
         }
     }
-    equations[2](0,6) = dy1;
+
+    equations2[2][6] = dy1;
 
     for(int i=0;i<6;i++){
-        if(5-i-1 > 0){
-            equations[3](0,i) = (5-i)*pow_x2[5-i-1];
+        if(5-i-1 >= 0){
+
+            equations2[3][i] =  (5-i)*pow_x2[5-i-1];
         }
         else{
-            equations[3](0,i) = 0;
+
+            equations2[3][i] = 0;
         }
     }
-    equations[3](0,6) = dy2;
+
+    equations2[3][6] = dy2;
 
     // second derivative equations
     for(int i=0;i<6;i++){
-        if(5-i-2 > 0){
-            equations[4](0,i) = (5-i)*(5-i-1)*pow_x1[5-i-2];
+        if(5-i-2 >= 0){
+
+            equations2[4][i] =  (5-i)*(5-i-1)*pow_x1[5-i-2];
         }
         else{
-            equations[4](0,i) = 0;
+
+            equations2[4][i] = 0;
         }
     }
-    equations[4](0,6) = ddy1;
+
+    equations2[4][6] = ddy1;
 
     for(int i=0;i<6;i++){
-        if(5-i-2 > 0){
-            equations[5](0,i) = (5-i)*(5-i-1)*pow_x2[5-i-2];
+        if(5-i-2 >= 0){
+
+            equations2[5][i] =  (5-i)*(5-i-1)*pow_x2[5-i-2];
         }
         else{
-            equations[5](0,i) = 0;
+
+            equations2[5][i] = 0;
         }
     }
-    equations[5](0,6) = ddy2;
 
+    equations2[5][6] = ddy2;
+
+    // cout<<"initially\n";
+    // for(int i=0;i<6;i++){
+    //     for(int j=0;j<7;j++){
+    //         // cout<<equations[i](0,j)<<" ";
+    //         cout<<equations2[i][j]<<" ";
+    //     }
+    //     cout<<"\n";
+    // }
+    // solving the equations
     for(int i=0;i<6;i++){
         for(int j=i+1;j<6;j++){
-            equations[j] = equations[j] - (equations[i]*( equations[j](0,i)/equations[i](0,i) ));
+
+            subtract_matrix(equations2,i,j);
         }
     }
+    // cout<<"iter 1\n";
+    // for(int i=0;i<6;i++){
+    //     for(int j=0;j<7;j++){
+    //         // cout<<equations[i](0,j)<<" ";
+    //         cout<<equations2[i][j]<<" ";
+    //     }
+    //     cout<<"\n";
+    // }
 
     for(int i=5;i>=0;i--){
         for(int j=i-1;j>=0;j--){
-            equations[j] = equations[j] - (equations[i]*( equations[j](0,i)/equations[i](0,i) ));
+
+            subtract_matrix(equations2,i,j);
         }
     }
 
+    // cout<<"new one\n";
+    // for(int i=0;i<6;i++){
+    //     for(int j=0;j<7;j++){
+    //         // cout<<equations[i](0,j)<<" ";
+    //         cout<<equations2[i][j]<<" ";
+    //     }
+    //     cout<<"\n";
+    // }
+
     double  coeff[6];
     for(int i=0;i<6;i++){
-        coeff[i] = equations[i](0,6)/equations[i](0,i);
+        coeff[i] = (equations2[i][6]*pow_x[5-i])/equations2[i][i];
     }
 
     double ans = 0;
 
-    for(int i=0;i<6;i++){
-        ans+= coeff[i]*pow_x[5-i];
+    for(int i=0;i<6;i++){        
+        ans+= coeff[i];
     }
     return ans;
 
 }
-// this function, given any x value, and a q values index in the dataset, interpolates along x
-double interpolate_x(double x, int q_i, int n, int flavour, int type){
-    int x_i[n];
-    int index1 = x_index(x);
-    for(int i=0;i<n;i++){
-        x_i[i] = return_index(index1, x_size-1, 0, i-1);
-    }
-    // x_i contains the index at which you'll find the x's you want
-    double a[n];
-    for(int i=0;i<n;i++){
-        a[i] = get_val(x_i[i],q_i,flavour);
-    }
-    // a contains the four values
-    double y1 = a[1];
-    double y2 = a[2];
-    double dy1,dy2;
-    if(x_i[0]==x_i[1]){
-        double dy1_2 = (a[2] - a[1]) / (x_arr[x_i[2]] - x_arr[x_i[1]]);
-        dy1 = dy1_2;
-    }
-    else{
-        double dy1_1 = (a[1] - a[0]) / (x_arr[x_i[1]] - x_arr[x_i[0]]);
-        double dy1_2 = (a[2] - a[1]) / (x_arr[x_i[2]] - x_arr[x_i[1]]);
-        dy1 = ((dy1_2*(x_arr[x_i[1]] - x_arr[x_i[0]]))/(x_arr[x_i[2]] - x_arr[x_i[0]])) + ((dy1_1*(x_arr[x_i[2]] - x_arr[x_i[1]]))/(x_arr[x_i[2]] - x_arr[x_i[0]]));
-    }
 
-    if(x_i[2]==x_i[3]){
-        double dy2_1 = (a[2] - a[1]) / (x_arr[x_i[2]] - x_arr[x_i[1]]);
-        dy2 = dy2_1;
-    }
-    else{
-        double dy2_2 = (a[3] - a[2]) / (x_arr[x_i[3]] - x_arr[x_i[2]]);
-        double dy2_1 = (a[2] - a[1]) / (x_arr[x_i[2]] - x_arr[x_i[1]]);
-        dy2 = ((dy2_2*(x_arr[x_i[2]] - x_arr[x_i[1]]))/(x_arr[x_i[3]] - x_arr[x_i[1]])) + ((dy2_1*(x_arr[x_i[3]] - x_arr[x_i[2]]))/(x_arr[x_i[3]] - x_arr[x_i[1]]));
-    }
-
-    // calculate second derivative
-    double ddy1, ddy2;
-    if(type==0){
-        // if type = 0, then we get second derivative with only four points
-        if(x_i[0]==x_i[1]){
-            ddy1 = 0;
-        }
-        else{
-            ddy1 = 2*(((a[2] - a[1]) / (x_arr[x_i[2]] - x_arr[x_i[1]])) - ((a[1] - a[0]) / (x_arr[x_i[1]] - x_arr[x_i[0]])))/(x_arr[x_i[2]] - x_arr[x_i[0]]);
-        }
-
-        if(x_i[2]==x_i[3]){
-            ddy2 = 0;
-        }
-        else{
-            ddy2 = 2*(((a[3] - a[2]) / (x_arr[x_i[3]] - x_arr[x_i[2]])) - ((a[2] - a[1]) / (x_arr[x_i[2]] - x_arr[x_i[1]])))/(x_arr[x_i[3]] - x_arr[x_i[1]]);
-        }
-        // cout<<"second\n";
-        // cout<<"second derivatives are - "<<ddy1<<" "<<ddy2<<"\n";
-    }
-    else if(type==1){
-        // we use six points to get the second derivative
-        int xo_i = return_index(index1, x_size-1, 0, -2);
-        int x6_i = return_index(index1, x_size-1, 0, 3);
-        // cout<<"index - ";
-        // cout<<xo_i<<" ";
-        // for(int i=0;i<4;i++){
-            // cout<<x_i[i]<<" ";
-        // }
-        // cout<<x6_i<<"\n";
-        double dd_x1,dd_x3;
-        if(xo_i==x_i[0] || x_i[0]==x_i[1]){
-            dd_x1 = 0;
-        }
-        else{
-            dd_x1 = 2*(((a[1] - a[0]) / (x_arr[x_i[1]] - x_arr[x_i[0]])) - ((a[0]-get_val(xo_i,q_i,0))/(x_arr[x_i[0]] - x_arr[xo_i])))/(x_arr[x_i[1]] - x_arr[xo_i]);
-        }
-        if(x_i[2]==x_i[3]){
-            dd_x3 = 0;
-        }
-        else{
-            dd_x3 = 2*(((a[3] - a[2]) / (x_arr[x_i[3]] - x_arr[x_i[2]])) - ((a[2] - a[1]) / (x_arr[x_i[2]] - x_arr[x_i[1]])))/(x_arr[x_i[3]] - x_arr[x_i[1]]);
-        }
-        ddy1 = ((dd_x1*(x_arr[x_i[2]]-x_arr[x_i[1]]))+(dd_x3*(x_arr[x_i[1]]-x_arr[x_i[0]])))/(x_arr[x_i[2]]-x_arr[x_i[0]]);
-
-
-        // for the second derivative as x3
-        double dd_x2,dd_x4;
-        if(x6_i==x_i[3] || x_i[3]==x_i[2]){
-            dd_x4 = 0;
-        }
-        else{
-            dd_x4 = (((get_val(x6_i,q_i,0)-a[3])/(x_arr[x6_i] - x_arr[x_i[3]])) - ((a[3] - a[2]) / (x_arr[x_i[3]] - x_arr[x_i[2]])))/((x_arr[x6_i] - x_arr[x_i[2]])/2);
-        }
-        if(x_i[0]==x_i[1]){
-            dd_x2 = 0;
-        }
-        else{
-            dd_x2 = 2*(((a[2] - a[1]) / (x_arr[x_i[2]] - x_arr[x_i[1]])) - ((a[1] - a[0]) / (x_arr[x_i[1]] - x_arr[x_i[0]])))/(x_arr[x_i[2]] - x_arr[x_i[0]]);
-        }
-        // cout<<dd_x2<<" "<<dd_x4<<" are the second derivatives things\n";
-        ddy2 = ((dd_x2*(x_arr[x_i[3]]-x_arr[x_i[2]]))+(dd_x4*(x_arr[x_i[2]]-x_arr[x_i[1]])))/(x_arr[x_i[3]]-x_arr[x_i[1]]);
-    }
-    // cout<<"second erivative for x\n";
-    // cout<<ddy1<<" "<<ddy2<<"\n";
-    
-    return solve_system(x,x_arr[x_i[1]],x_arr[x_i[2]],y1,y2,dy1,dy2,ddy1,ddy2);
-}
-// this function, given any q value, and the values that were gained from interpolation in the x direction, 
-// gives the final interpolation value
-double interpolate_q(double q, double* values , int n, int type){
-    int q_i[4];
-    int index1 = q_index(q);
-    for(int i=0;i<4;i++){
-        q_i[i] = return_index(index1, q_size-1, 0, i-1);
-    }
-    double a[4];
-    // cout<<"values are (2):\n";
-    for(int i=0;i<4;i++){
-        a[i] = values[i];
-        // cout<<a[i]<<" ";
-    }
-    if(type==1){
-        for(int i=0;i<4;i++){
-            a[i] = values[i+1];
-        // cout<<a[i]<<" ";
-        }   
-    }
-    // a contains the four values
-    double y1 = a[1];
-    double y2 = a[2];
-    double dy1,dy2;
-    if(q_i[0]==q_i[1]){
-        double dy1_2 = (a[2] - a[1]) / (q_arr[q_i[2]] - q_arr[q_i[1]]);
-        dy1 = dy1_2;
-    }
-    else{
-        double dy1_1 = (a[1] - a[0]) / (q_arr[q_i[1]] - q_arr[q_i[0]]);
-        double dy1_2 = (a[2] - a[1]) / (q_arr[q_i[2]] - q_arr[q_i[1]]);
-        dy1 = ((dy1_2*(q_arr[q_i[1]] - q_arr[q_i[0]]))/(q_arr[q_i[2]] - q_arr[q_i[0]])) + ((dy1_1*(q_arr[q_i[2]] - q_arr[q_i[1]]))/(q_arr[q_i[2]] - q_arr[q_i[0]]));
-    }
-
-    if(q_i[2]==q_i[3]){
-        double dy2_1 = (a[2] - a[1]) / (q_arr[q_i[2]] - q_arr[q_i[1]]);
-        dy2 = dy2_1;
-    }
-    else{
-        double dy2_2 = (a[3] - a[2]) / (q_arr[q_i[3]] - q_arr[q_i[2]]);
-        double dy2_1 = (a[2] - a[1]) / (q_arr[q_i[2]] - q_arr[q_i[1]]);
-        dy2 = ((dy2_2*(q_arr[q_i[2]] - q_arr[q_i[1]]))/(q_arr[q_i[3]] - q_arr[q_i[1]])) + ((dy2_1*(q_arr[q_i[3]] - q_arr[q_i[2]]))/(q_arr[q_i[3]] - q_arr[q_i[1]]));
-    }
-
-    // calculate second derivative
-    double ddy1, ddy2;
-    if(type==0){
-        // if type = 0, then we get second derivative with only four points
-        if(q_i[0]==q_i[1]){
-            ddy1 = 0;
-        }
-        else{
-            ddy1 = 2*(((a[2] - a[1]) / (q_arr[q_i[2]] - q_arr[q_i[1]])) - ((a[1] - a[0]) / (q_arr[q_i[1]] - q_arr[q_i[0]])))/(q_arr[q_i[2]] - q_arr[q_i[0]]);
-        }
-
-        if(q_i[2]==q_i[3]){
-            ddy2 = 0;
-        }
-        else{
-            ddy2 = 2*(((a[3] - a[2]) / (q_arr[q_i[3]] - q_arr[q_i[2]])) - ((a[2] - a[1]) / (q_arr[q_i[2]] - q_arr[q_i[1]])))/(q_arr[q_i[3]] - q_arr[q_i[1]]);
-        }
-    }
-    else if(type==1){
-        // we use siq points to get the second derivative
-        int qo_i = return_index(index1, q_size-1, 0, -2);
-        int q6_i = return_index(index1, q_size-1, 0, 3);
-        double dd_q1,dd_q3;
-        if(qo_i==q_i[0] || q_i[0]==q_i[1]){
-            dd_q1 = 0;
-        }
-        else{
-            dd_q1 = 2*(((a[1] - a[0]) / (q_arr[q_i[1]] - q_arr[q_i[0]])) - ((a[0]-values[0])/(q_arr[q_i[0]] - q_arr[qo_i])))/(q_arr[q_i[1]] - q_arr[qo_i]);
-        }
-        if(q_i[2]==q_i[3]){
-            dd_q3 = 0;
-        }
-        else{
-            dd_q3 = 2*(((a[3] - a[2]) / (q_arr[q_i[3]] - q_arr[q_i[2]])) - ((a[2] - a[1]) / (q_arr[q_i[2]] - q_arr[q_i[1]])))/(q_arr[q_i[3]] - q_arr[q_i[1]]);
-        }
-        ddy1 = ((dd_q1*(q_arr[q_i[2]]-q_arr[q_i[1]]))+(dd_q3*(q_arr[q_i[1]]-q_arr[q_i[0]])))/(q_arr[q_i[2]]-q_arr[q_i[0]]);
-
-
-        // for the second derivative as q3
-        double dd_q2,dd_q4;
-        if(q6_i==q_i[3] || q_i[3]==q_i[2]){
-            dd_q4 = 0;
-        }
-        else{
-            dd_q4 = -2*(((a[3] - a[2]) / (q_arr[q_i[3]] - q_arr[q_i[2]])) - ((values[5]-a[3])/(q_arr[q_i[0]] - q_arr[q6_i])))/(q_arr[q6_i] - q_arr[q_i[3]]);
-        }
-        if(q_i[0]==q_i[1]){
-            dd_q2 = 0;
-        }
-        else{
-            dd_q2 = 2*(((a[2] - a[1]) / (q_arr[q_i[2]] - q_arr[q_i[1]])) - ((a[1] - a[0]) / (q_arr[q_i[1]] - q_arr[q_i[0]])))/(q_arr[q_i[2]] - q_arr[q_i[0]]);
-        }
-        ddy2 = ((dd_q2*(q_arr[q_i[3]]-q_arr[q_i[2]]))+(dd_q4*(q_arr[q_i[2]]-q_arr[q_i[1]])))/(q_arr[q_i[3]]-q_arr[q_i[1]]);
-    }
-
-    
-    return solve_system(q,q_arr[q_i[1]],q_arr[q_i[2]],y1,y2,dy1,dy2,ddy1,ddy2);
-    
-}
-
-// --------------------------------------------- NEW SHIT
-double six_point_inter(double x, double* values, double* x_val){
-    double a[6];
-    for(int i=0;i<6;i++){
-        a[i] = values[i];
-    }
+// this is the code for the 6-point interpolator
+double six_point_inter(double x, double* a, double* x_val){
     double y1 = a[2];
     double y2 = a[3];
     double dy1,dy2;
+    double dy_[5];
+    for(int i=0;i<5;i++){
+        dy_[i] = (a[i+1] - a[i])/(x_val[i+1]-x_val[i]);
+    }
+
+
+
+    dy1 = ((dy_[2]*(x_val[2] - x_val[1])) + (dy_[1]*(x_val[3] - x_val[2])))/(x_val[3] - x_val[1]);
+
+
+    dy2 = ((dy_[3]*(x_val[3] - x_val[2])) + (dy_[2]*(x_val[4] - x_val[3])))/(x_val[4] - x_val[2]);
+
     
-    double dy1_1 = (a[2] - a[1]) / (x_val[2] - x_val[1]);
-    double dy1_2 = (a[3] - a[2]) / (x_val[3] - x_val[2]);
-    dy1 = ((dy1_2*(x_val[2] - x_val[1]))/(x_val[3] - x_val[1])) + ((dy1_1*(x_val[3] - x_val[2]))/(x_val[3] - x_val[1]));
-
-    double dy2_2 = (a[4] - a[3]) / (x_val[4] - x_val[3]);
-    double dy2_1 = (a[3] - a[2]) / (x_val[3] - x_val[2]);
-    dy2 = ((dy2_2*(x_val[3] - x_val[2]))/(x_val[4] - x_val[2])) + ((dy2_1*(x_val[4] - x_val[3]))/(x_val[4] - x_val[2]));
-
-    // cout<<"index - ";
-    // cout<<x_i[0]<<" ";
-    // for(int i=0;i<4;i++){
-        // cout<<x_i[i]<<" ";
-    // }
-    // cout<<x_i[5]<<"\n";
     double ddy1, ddy2;
     double dd_x1,dd_x3;
-    dd_x1 = 2*(((a[2] - a[1]) / (x_val[2] - x_val[1])) - ((a[1]-a[0])/(x_val[1] - x_val[0])))/(x_val[2] - x_val[0]);
-    dd_x3 = 2*(((a[4] - a[3]) / (x_val[4] - x_val[3])) - ((a[3] - a[2]) / (x_val[3] - x_val[2])))/(x_val[4] - x_val[2]);
+    dd_x1 = (2*((dy_[1]) - (dy_[0])))/(x_val[2] - x_val[0]);
+    dd_x3 = (2*((dy_[3]) - (dy_[2])))/(x_val[4] - x_val[2]);
 
     ddy1 = ((dd_x1*(x_val[3]-x_val[2]))+(dd_x3*(x_val[2]-x_val[1])))/(x_val[3]-x_val[1]);
 
 
     // for the second derivative as x3
     double dd_x2,dd_x4;
-    dd_x4 = (((a[5]-a[4])/(x_val[5] - x_val[4])) - ((a[4] - a[3]) / (x_val[4] - x_val[3])))/((x_val[5] - x_val[3])/2);
-    dd_x2 = 2*(((a[3] - a[2]) / (x_val[3] - x_val[2])) - ((a[3] - a[2]) / (x_val[2] - x_val[1])))/(x_val[3] - x_val[1]);
+    dd_x4 = (2*((dy_[4]) - (dy_[3])))/((x_val[5] - x_val[3]));
+    dd_x2 = (2*((dy_[2]) - (dy_[1])))/(x_val[3] - x_val[1]);
     
     // cout<<dd_x2<<" "<<dd_x4<<" are the second derivatives things\n";
-    ddy2 = ((dd_x2*(x_val[4]-x_val[3]))+(dd_x4*(x_val[3]-x_val[2])))/(x_val[4]-x_val[2]);
+    ddy2 = ( (dd_x2*(x_val[4]-x_val[3])) + (dd_x4*(x_val[3]-x_val[2])) )/(x_val[4]-x_val[2]);
 
     return solve_system(x,x_val[2],x_val[3],y1,y2,dy1,dy2,ddy1,ddy2);
 }
 
+// this is the code for the linear interpolator
 double linear_inter(double x, double* values, double* x_val){
     double y1 = values[0];
     double y2 = values[1];
     double x1 = x_val[0];
     double x2 = x_val[1];
-    return y1 + (y2-y1)*(x-x1)/(x2-x1);
+    return y1 + ((y2-y1)*(x-x1))/(x2-x1);
 }
-
+// this is the code for the 4-point interpolator
 double four_point_inter(double x, double* values, double* x_val){
     double a[4];
     for(int i=0;i<4;i++){
@@ -464,6 +308,7 @@ double four_point_inter(double x, double* values, double* x_val){
 // this is the main function from which all interpolation is called
 double interpolate(double x, double q, int flavour, int type){
     int x_i = x_index(x);
+    
     int q_i = q_index(q);
     int n = 4;
     int x_a[n],q_a[n];
@@ -595,7 +440,7 @@ double interpolate(double x, double q, int flavour, int type){
 
 }
 
-  int main(){
+int main(){
 
     fstream fin;
 
@@ -702,7 +547,7 @@ double interpolate(double x, double q, int flavour, int type){
 
     double x_val = -20;
     double q_val = 0.5;
-    // cout<<interpolate((-0.4),(1),0,1)<<"\n";
+    // cout<<interpolate((-16.4),(1.2),0,1)<<"\n";
     MyFile<<"lnx,lnq,xf\n";
     while(q_val<10){
         x_val = -18;
@@ -715,6 +560,7 @@ double interpolate(double x, double q, int flavour, int type){
         cout<<q_val<<"\n";
     }
     MyFile.close();
+
 
 
 }
